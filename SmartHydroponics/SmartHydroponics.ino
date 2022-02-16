8af0dbcadbe74438c83a826116fab41f22c48eae
dQuigz <dquigz89@gmail.com>
2022-02-16 14:22:43 -0500

    Create SmartHydroponics.ino

---
+++ SmartHydroponics/SmartHydroponics.ino
@@ -0,0 +1,24 @@
+//SmartMicroponics system by Dennis Quigley
+//This file is essentially just a header for arduino, with everything really starting in SmartMP.h
+//Initialize 
+//TODO: Move everything else to .h
+
+#include "DualBMP.h"
+#include "ProgMemStr.h"
+#include "SmartMP.h"
+const PROGMEM char initSMMes[] = "\nInitializing SmartMicroponics System...";
+
+
+DualBMP dualbmp;
+SmartMP smartmp;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {}; //wait for serial connection to establish
+  ProgMemStr().printCharMes(initSMMes);//TODO: Create an object of ProgMemStr and pass it to the other modules
+  smartmp.SMP_init(&dualbmp);//initilize Smart Microponics Data and Interface systems, pass DualBMP object
+  
+}
+void loop() {
+  smartmp.SMP_loop();
+}
